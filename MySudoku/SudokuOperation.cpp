01143299e1bd96637e8557007b718d5413516475
Yixuan Guo <2761683494@qq.com>
2018-12-04 20:44:09 +0800

    添加了“行移动步长”模块

--- MySudoku/SudokuOperation.cpp
+++ MySudoku/SudokuOperation.cpp
@@ -1,14 +1,49 @@
 #include "stdafx.h"
 #include "SudokuOperation.h"
+#include <string.h>
 using namespace std;
 
 SudokuOperation::SudokuOperation()
 {
+	memset(move_step_matrix, 0, sizeof(move_step_matrix));
+	memset(result_matrix, 0, sizeof(result_matrix));
+
+}
+
+void SudokuOperation::move_step_generate()
+{
+	int move_dictionary1[2][3] = { { 0,3,6 },{ 0,6,3 } };
+	int move_dictionary2[6][3] = { { 1,4,7 },{ 1,7,4 },{ 4,1,7 },{ 4,7,1 },{ 7,4,1 },{ 7,1,4 } };
+	int move_dictionary3[6][3] = { { 2,5,8 },{ 2,8,5 },{ 5,2,8 },{ 5,8,2 },{ 8,2,5 },{ 8,5,2 } };
+	int i, j, k, count = 0;
+	int step[9];
+	for (i = 0; i < 2; i++)
+	{
+		for (j = 0; j < 6; j++)
+		{
+			for (k = 0; k < 6; k++)
+			{
+				memcpy(step, move_dictionary1[i], 3*sizeof(int));
+				memcpy(&step[3], move_dictionary2[j], 3*sizeof(int));
+				memcpy(&step[6], move_dictionary3[k], 3*sizeof(int));
+				memcpy(&move_step_matrix[count], step, sizeof(int));
+				count++;
+			}
+		}
+	}
+		
 }
 
 void SudokuOperation::generate_ending(int num)
 {
+	//To test if method generate_ending is recalled.
 	//printf("%d endings need to be generated!\n", num);
+
+	//double the first line of first Sudoku output, and joint them together
+	int first_line[9] = { 9,1,2,3,4,5,6,7,8 };
+	int joint_line[18] = { 9,1,2,3,4,5,6,7,8,9,1,2,3,4,5,6,7,8 };
+	
+	
 
 }
 
--- MySudoku/SudokuOperation.h
+++ MySudoku/SudokuOperation.h
@@ -7,12 +7,23 @@ public:
 	//initialize the SudokuOperation class
 	SudokuOperation();
 
+	//generate how many steps each line move, put the result into move_step_matrix
+	void move_step_generate();
+
+	//generate the first line of game ending
+	void first_line_generate(int num);
+
 	//generate the end of the game if '-c' and an integar are fetched from cmd window
 	void generate_ending(int num);
 
 	//solve the Sudoku game if '-s' and filename is fetched from cmd window
 	void solve_sudoku();
 
+
 private:
+	//The number of Sudoku now
+	int skd_num_now;
+	int move_step_matrix[72][9];
+	int result_matrix[1000000][9][9];
 
 };
